ae96934f354c541512843c1efa925b759dae4551
ryrylleee <153406870+ryrylleee@users.noreply.github.com>
2024-10-04 13:56:11 +0800

    Update GetArrayMean.java

--- Laboratory Activities/Laboratory Activty 2-04-10-24/GetArrayMean.java
+++ Laboratory Activities/Laboratory Activty 2-04-10-24/GetArrayMean.java
@@ -1,1 +1,27 @@
+import java.util.Scanner;
 
+public class GetArrayMean {
+    public static double getArrayMean(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return (double) sum / array.length;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = scanner.nextInt();
+
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter number: ");
+            numbers[i] = scanner.nextInt();
+        }
+
+        double mean = getArrayMean(numbers);
+        System.out.printf("Mean of array is: %.2f", mean);
+    scanner.close();
+    }
+}
