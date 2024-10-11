450ac87dd3093b85ec0a93ede8f323d5084d0b58
ryrylleee <153406870+ryrylleee@users.noreply.github.com>
2024-10-11 15:02:56 +0800

    Create Car.java

---
+++ Exercise Sets/Exercise - Encapsulation/Car.java
@@ -0,0 +1,54 @@
+public class Car {
+//PRIVATE PROPERTIES
+    private String color;
+    private double price;
+    private char size;
+
+//CONSTRUCTOR
+    public Car(String color, double price, char size) {
+        this.color = color;
+        this.price = price;
+        this.size = Character.toUpperCase(size);
+    }
+
+//GETTER METHOD
+    public String getColor() {
+        return color;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public char getSize() {
+        return size;
+    }
+
+//SETTER METHOD
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public void setSize(char size) {
+        this.size = Character.toUpperCase(size);
+    }
+
+//toString () METHOD
+    public String toString() {
+        String sizeDescriptor;
+        switch (size) {
+            case 'S':
+                sizeDescriptor = "small";
+                break;
+            case 'M':
+                sizeDescriptor = "medium";
+                break;
+            case 'L':
+                sizeDescriptor = "large";
+                break;
+            default:
+                sizeDescriptor = "unknown";
+        }
+        return String.format("Car (%s) - P%.2f - %s", color, price, sizeDescriptor);
+    }
+}
